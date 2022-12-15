7b73285c66ebdfeb998274d1c343c84d5719e46b
LeroyK <785632486@qq.com>
2022-12-15 21:31:25 +0800

    2022年12月15日

--- 教程版/v6router/src/views/Film.jsx
+++ 教程版/v6router/src/views/Film.jsx
@@ -1,6 +1,9 @@
 // @ts-nocheck
 import React from "react";
+// 如果要使用css模块导入语法，依然需要.module.css后缀
 import style from "./css/film.module.css";
+// import style from "./css/film.css";
+
 
 // 嵌套子路由
 import { NavLink, Outlet } from "react-router-dom";
