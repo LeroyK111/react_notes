1c8f1a6cb3a5bae45654a3db32c6907982cad062
LeroyK <785632486@qq.com>
2023-05-31 13:14:23 +0800

    1

--- myApp/src/page/Home.tsx
+++ myApp/src/page/Home.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-
+import React, { useEffect,useState } from "react";
 
 export default function home() {
-  return (
-    <div>
-      this is home page
-    </div>
-  );
+  const [list, setList] = useState([]);
+
+  const fetchList = () => {
+    // Simulate fetching data from the server via setTimeout
+    setTimeout(() => {
+      setList([]);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return <div>
+    <p>this is home page</p> 
+    <div>测试一下&&运算符</div>
+    {/* &&则会出现0 */}
+    {list.length && <div>1111</div>}
+    {/* 转换成bool值，则它不会出现0 */}
+    {!!list.length && <div>1111</div>}
+    {/* 使用明确的判断 */}
+    {list.length>=1 && <div>1111</div>}
+    {/* 使用三元表达式 */}
+    {list.length?<div>1111</div>:null}
+  </div>;
 }
