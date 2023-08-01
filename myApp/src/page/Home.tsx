bac93b05a83e18a78e8375656f885dc9bff82202
LeroyK <bridieiybnf@gmail.com>
2023-08-01 21:14:59 +0800

    学习新语法

--- myApp/src/page/Home.tsx
+++ myApp/src/page/Home.tsx
@@ -18,7 +18,7 @@ export default function home() {
     <p>this is home page</p> 
     <div>测试一下&&运算符</div>
     {/* &&则会出现0 */}
-    {list.length && <div>1111</div>}
+    {list.length > 0 && <div>1111</div>}
     {/* 转换成bool值，则它不会出现0 */}
     {!!list.length && <div>1111</div>}
     {/* 使用明确的判断 */}
