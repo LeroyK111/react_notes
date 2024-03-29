1517ae1f564b7427a8bc3c80766b49f689640da8
LeroyK <bridieiybnf@gmail.com>
2024-03-29 22:03:58 +0800

    1

--- DrCow/src/routes/__root.tsx
+++ DrCow/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { createRootRoute } from "@tanstack/react-router";
 import React, { Suspense } from "react";
 import App from "../App";
-import { css } from "@emotion/react";
 
 // 开发中使用
 const TanStackRouterDevtools =
@@ -16,9 +15,6 @@ const TanStackRouterDevtools =
         }))
       );
 
-
-
-
 export const Route = createRootRoute({
   component: () => (
     <>
@@ -27,7 +23,7 @@ export const Route = createRootRoute({
       <App></App>
       {/* 直到加载完毕 */}
       <Suspense>
-        {/* <TanStackRouterDevtools  /> */}
+        <TanStackRouterDevtools />
       </Suspense>
     </>
   ),
